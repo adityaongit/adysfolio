779e9fd1eb0b6043db9e7051f5b2d5330d709c08
Aditya Jindal <raghavjindal017@gmail.com>
2023-12-29 22:15:47 +0530

    update: footer init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 function App() {
     return (
         <div className="sm:mx-26 mx-6 md:mx-24 lg:mx-[250px]">
             <Header />
+            <Footer />
         </div>
     );
 }
