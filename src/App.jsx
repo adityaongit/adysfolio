9f2703e1341019ab0154ac75a855e8169428233c
Aditya Jindal <raghavjindal017@gmail.com>
2023-12-29 22:22:50 +0530

    update: icons added & Hero Upadted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/hero";
 function App() {
     return (
         <div className="sm:mx-26 mx-6 md:mx-24 lg:mx-[250px]">
             <Header />
+            <Hero />
             <Footer />
         </div>
     );
