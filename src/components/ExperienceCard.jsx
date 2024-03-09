186324fbf41afa7a0b992ffa72e65f17c69beb42
Aditya Jindal <raghavjindal017@gmail.com>
2024-03-09 15:41:10 +0530

    Experience section updated

---
+++ src/components/ExperienceCard.jsx
@@ -0,0 +1,84 @@
+const ExperienceCard = ({ data }) => {
+    console.log(data);
+
+    const URL = "https://adysfolio.onrender.com";
+    const companyLogo = data.companyLogo.data.attributes.formats.thumbnail.url;
+
+    const { companyName, startDate, endDate, Role, description, skillTag } =
+        data;
+
+    function formatDateRange(startDate, endDate) {
+        // Parse start and end dates
+        const startDateObj = new Date(startDate);
+        const endDateObj = new Date(endDate);
+
+        // Get month and year for start and end dates
+        const startMonth = startDateObj.toLocaleString("default", {
+            month: "long",
+        });
+        const startYear = startDateObj.getFullYear();
+        const endMonth = endDateObj.toLocaleString("default", {
+            month: "long",
+        });
+        const endYear = endDateObj.getFullYear();
+
+        // Format the date range
+        const formattedDateRange = `${startMonth} ${startYear} - ${endMonth} ${endYear}`;
+
+        return formattedDateRange;
+    }
+
+    return (
+        <div>
+            <div className="intern1 shadow-md rounded-xl p-6 bg-gray-50">
+                <img className="h-32 mx-auto w-1/2" src={URL + companyLogo} />
+
+                <div className="my-auto mt-5">
+                    <p className="font-semiboldld text-lg mt-4">
+                        {companyName}
+                    </p>
+                    <p className="mt-1 text-gray-600">
+                        {formatDateRange(startDate, endDate)}
+                    </p>
+                </div>
+                <p className="mt-2 font-medium text-gray-600 text-lg">{Role}</p>
+
+                <ul>
+                    {description.map((singleDes, index) => {
+                        return (
+                            <div key={index} className="">
+                                <li className="flex items-start my-2">
+                                    <svg
+                                        className="w-3.5 h-3.5 mr-2 mt-[6px] text-green-500 dark:text-green-400 flex-shrink-0"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    {singleDes.children[0].text}
+                                </li>
+                            </div>
+                        );
+                    })}
+                </ul>
+
+                <div className="pt-4 pb-2">
+                    {skillTag.map((skill) => {
+                        return (
+                            <span
+                                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-[550px] text-gray-700 mr-2 mb-2"
+                                key={skill.children[0].text}
+                            >
+                                {skill.children[0].text}
+                            </span>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ExperienceCard;
--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,7 +1,42 @@
-const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+import { useState } from "react";
+import { useEffect } from "react";
+import ExperienceCard from "./ExperienceCard.jsx";
 
-export default Experience
+const Experience = () => {
+    const [experiences, setExperiences] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const data = await fetch(
+            "https://adysfolio.onrender.com/api/experiences?populate=*"
+        );
+        const json = await data.json();
+
+        console.log(json.data);
+        setExperiences(json.data);
+    }
+
+    return (
+        <div>
+            <div id="experience" className="container mx-auto mt-16">
+                <h1 className="text-3xl font-bold mb-4">Experience</h1>
+                <p className="mb-10">
+                    Some of the <span id="intern">Internships</span> that
+                    I&apos;ve did:
+                </p>
+            </div>
+            <div className="projects grid gap-12 lg:grid-cols-2">
+                {experiences.map((exp) => {
+                    return (
+                        <ExperienceCard key={exp.id} data={exp.attributes} />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
